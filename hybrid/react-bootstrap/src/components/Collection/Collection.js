9192ce61514bcbfd6ab4167a94ac5d88b984a8d2
piotrek <piotr@tipser.com>
2021-10-13 15:36:42 +0200

    Fixed build errors

--- hybrid/react-bootstrap/src/components/Collection/Collection.js
+++ hybrid/react-bootstrap/src/components/Collection/Collection.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useTipserContext} from "@tipser/tipser-elements";
 import {ProductTile} from "../ProductTile/ProductTile";
 
